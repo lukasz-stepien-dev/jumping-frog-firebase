657187bf943508c5d08f5920e72419c6bf8fad32
Łukasz Stępień <lukaszatepien@gmail.com>
2023-12-05 15:36:14 +0100

    JUMPING!!!!!

--- src/Frog.jsx
+++ src/Frog.jsx
@@ -1,7 +1,16 @@
 import frogImg from './img/frog.png';
 import './css/Frog.css';
+import {useState} from "react";
 export default function Frog() {
+    const [isJump, setJump] = useState('');
+
+    function jump() {
+        setJump('jump');
+        setTimeout(() => setJump(''), 500)
+    }
+
+
     return (
-        <img src={frogImg} alt="frog" />
+        <img onClick={jump} src={frogImg} className={isJump} alt="frog" />
     )
 }
