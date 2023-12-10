02a240d0637d4948e8a452f1302474e4d8c2e6b6
Łukasz Stępień <lukaszatepien@gmail.com>
2023-12-10 01:16:49 +0100

    Changing text in a button to the input with link and copy button

--- src/ShareButton.jsx
+++ src/ShareButton.jsx
@@ -1,7 +1,20 @@
 import "./css/ShareButton.css";
+import { useState } from "react";
+import React from 'react';
 
 export default function ShareButton() {
+    const [btnContent, setBtnContent] = useState('Jump with a friend!');
+    function share() {
+        setBtnContent(() => {return (
+            <React.Fragment>
+                <input readOnly={true} id={"link"} type="text"/>
+                <button id={"copy"}>Copy</button>
+            </React.Fragment>
+        )})
+    }
+
+
     return (
-        <button id={"share-button"}><p>Jump with a friend!</p></button>
+        <button onClick={share} id={"share-button"}>{btnContent}</button>
     )
 }
