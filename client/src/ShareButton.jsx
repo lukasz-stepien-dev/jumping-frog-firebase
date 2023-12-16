36af9a0f928d2b7269705e516dc96a0432bbabe0
Łukasz Stępień <lukaszatepien@gmail.com>
2023-12-16 11:42:26 +0100

    Revert "Changing text in a button to the input with link and copy button"
    This reverts commit 02a240d0637d4948e8a452f1302474e4d8c2e6b6.

--- client/src/ShareButton.jsx
+++ client/src/ShareButton.jsx
@@ -1,20 +1,7 @@
 import "./css/ShareButton.css";
-import { useState } from "react";
-import React from 'react';
 
 export default function ShareButton() {
-    const [btnContent, setBtnContent] = useState('Jump with a friend!');
-    function share() {
-        setBtnContent(() => {return (
-            <React.Fragment>
-                <input readOnly={true} id={"link"} type="text"/>
-                <button id={"copy"}>Copy</button>
-            </React.Fragment>
-        )})
-    }
-
-
     return (
-        <button onClick={share} id={"share-button"}>{btnContent}</button>
+        <button id={"share-button"}><p>Jump with a friend!</p></button>
     )
 }
